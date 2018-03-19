743c620bd9a5dd11b26ab7a9f631d919167a3b64
Nikil K <nikilok@gmail.com>
2018-03-19 08:32:02 +0400

    Fixed end to end tests

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('property-app App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Main content should exist with a class called main-content', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getClassFromElement()).toEqual('main-content');
   });
 });
